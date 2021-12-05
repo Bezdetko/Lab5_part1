bb35c5bd52e0b7db2ec372ae22e46b090f128ff0
Bezdetko <bezdetk0@mail.ru>
2021-12-05 21:30:28 +0300

    версия 1

--- Lab5_part1/src/ru/avalon/javapp/devj110/halfDivisionMethod/F4.java
+++ Lab5_part1/src/ru/avalon/javapp/devj110/halfDivisionMethod/F4.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package ru.avalon.javapp.devj110.halfDivisionMethod;
 
-/**
- *
- * @author bezdetk0@mail.ru
- */
+
 public class F4 {
         protected double tang(double x) {
         return Math.tan(x);
--- Lab5_part1/src/ru/avalon/javapp/devj110/halfDivisionMethod/Main.java
+++ Lab5_part1/src/ru/avalon/javapp/devj110/halfDivisionMethod/Main.java
@@ -1,14 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package ru.avalon.javapp.devj110.halfDivisionMethod;
 
-
-/**
- *
- * @author bezdetk0@mail.ru
- */
 public class Main {
 private static final double EPS = 1e-5;
 
